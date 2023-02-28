cae26af665eb4aa5db9d392e4043258562511719
Djenaba <djenabacooper@gmail.com>
2023-02-28 00:11:22 -0600

    Added supers app

--- super_types/views.py
+++ super_types/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import SuperType
+from .serializers import SuperTypeSerializer
 
-# Create your views here.
+#@api_view(['GET'])
+#def super_list(request):
+    
+#    if request.method == 'GET':
+ #       supers =  
+ 
