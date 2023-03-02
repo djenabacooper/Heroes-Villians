450fa0645a6784ed2e7b045d02f0b03089b8f052
Djenaba <djenabacooper@gmail.com>
2023-03-02 00:59:37 -0600

    Added supers list view + super detail view

--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Super
+from .serializers import SuperSerializer
 
-# Create your views here.
+@api_view(['GET'])
+def super_list(request):
+    
+    if request.method == 'GET':
+        supers =  Super.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
+    
+@api_view(['GET'])
+def super_detail(request, pk):
+
+    if request.method == 'GET':
+        super = get_object_or_404(Super, pk=pk)
+        serializer = SuperSerializer(super)
+        return Response(serializer.data)
+    
+
+ 
